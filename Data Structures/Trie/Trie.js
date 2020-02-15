77c33a10e1f5deb3aa33e152fda41f3a0ce70542
Abhi <abhiramani3@gmail.com>
2020-02-15 19:57:48 +0530

    add insert

---
+++ Data Structures/Trie/Trie.js
@@ -0,0 +1,34 @@
+/**
+ * A Trie/Prefix Tree is a kind of search tree used to provide quick lookup
+ * of words/patterns in a set of words. A basic Trie however has O(n^2) space complexity
+ * making it impractical in practice. It however provides O(max(search_string, length of longest word)) 
+ * lookup time making it an optimal approach when space is not an issue.
+ */
+
+var TrieNode = (function () {
+
+    function TrieNode() {
+        this.nodes = {};
+        this.isLeaf = false;
+    }
+
+    // Inserts a word into the Trie
+    TrieNode.prototype.insert = function (word) {
+        let curr = this;
+        word.split("").forEach(char => {
+            if (!(char in curr.nodes)) {
+                curr.nodes[char] = new TrieNode();
+            }
+            curr = curr.nodes[char];
+        });
+        curr.isLeaf = true;
+    };
+    return TrieNode;
+}());
+
+
+
+//Implementation of BST
+var trie = new TrieNode();
+trie.insert("HELLO");
+
